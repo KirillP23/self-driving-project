b2af88fd24d2d8f7e045f805206aab0cc8654dd3
KirillP23 <kir.paramonov@gmail.com>
2022-08-21 18:29:34 +0400

    Updated report again

--- data.py
+++ data.py
@@ -20,6 +20,26 @@ class Udacity(Dataset):
         if self.transform:
             centre_image = self.transform(centre_image)
         centre_steering = float(self.df_data.iloc[idx][3])
+        return centre_image, centre_steering
+    
+    
+class UdacityAugmented(Dataset):
+    def __init__(self, data_path, df_data, transform):
+        self.df_data = df_data
+        self.data_path = data_path
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df_data)*2
+
+    def __getitem__(self, idx):
+        centre_image = np.array(Image.open((str(Path(self.data_path, self.df_data.iloc[idx//2][0])))))
+        if idx%2 == 1:
+            centre_image = np.copy(np.flip(centre_image, axis=1))
+        if self.transform:
+            centre_image = self.transform(centre_image)
+        centre_steering = float(self.df_data.iloc[idx//2][3])
         if idx % 2 == 1:
             centre_steering = -centre_steering
         return centre_image, centre_steering
+    
